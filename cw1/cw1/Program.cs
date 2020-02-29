06404e7c7d002fdbe5d5aa6c99cf12959e3406e3
Bartłomiej Więcek <s17718@pjwstk.edu.pl>
2020-02-29 19:39:23 +0100

    cw1 zad 2

---
+++ cw1/cw1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace cw1
+{
+    class Program
+    {
+        public static async System.Threading.Tasks.Task Main(string[] args)
+        {
+
+            var client = new System.Net.Http.HttpClient();
+            var result = await client.GetAsync("https://www.pja.edu.pl");
+
+            if (result.IsSuccessStatusCode)
+            {
+                string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]", RegexOptions.IgnoreCase);
+
+                var matches = regex.Matches(html);
+
+                foreach (var m in matches) Console.WriteLine(m.ToString());
+            }
+
+
+
+            Console.WriteLine("...");
+        }
+    }
+}
